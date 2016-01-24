5bc4370347b39fc5ca50725ab812fe69ea3fd6fd
ranrozin <ran.rozin@gmail.com>
2016-01-24 12:05:29 +0200

    changed the log path to be the assembly one and not EnRoute one

--- bright/Log.cs
+++ bright/Log.cs
@@ -9,10 +9,12 @@ namespace bright
 {
     class Log
     {
-        static string _localLogFile = "log.txt";
+        static string _localLogFile = "brightLog.txt";
         public static void AddEntry(string line)
         {
-            string filePath = Path.Combine(Directory.GetCurrentDirectory(), _localLogFile);
+            //string filePath = Path.Combine(Directory.GetCurrentDirectory(), _localLogFile);
+            var dir = AppDomain.CurrentDomain.BaseDirectory;
+            string filePath = Path.Combine(dir, _localLogFile);
             using (StreamWriter writer = new StreamWriter(filePath, true, Encoding.UTF8))
             {
                 writer.WriteLine(string.Format("time: {0} - {1}",DateTime.UtcNow, line));
